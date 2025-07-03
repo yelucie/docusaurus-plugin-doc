d12a61e0c31555a8ea916fc7de5832348b833892
YE Lucie <lucie.ye@audemarspiguet.com>
2025-07-03 12:38:36 +0200

    feat: #0 update config for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://yelucie.github.io/docusaurus-plugin-doc",
+  url: "https://yelucie.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docusaurus-plugin-doc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
